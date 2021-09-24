8c8b8be14fa59a1d26e2873528d5b0ed187d5229
Nyx <fauzi.dohc@yahoo.co.id>
2021-09-24 12:43:31 +0700

    ench
    tambahan fitur

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import "assets/scss/style.scss";
 
 import LandingPage from "pages/LandingPage";
 import Example from "pages/Example";
+import DetailsPage from "pages/DetailsPage";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Route path="/" component={LandingPage}></Route>
-        <Route path="/example" component={Example}></Route>
+        <Switch>
+          <Route path="/" exact component={LandingPage}></Route>
+          <Route path="/properties/:id" exact component={DetailsPage}></Route>
+          <Route path="/example" exact component={Example}></Route>
+        </Switch>
       </Router>
     </div>
   );
--- src/parts/Hero.js
+++ src/parts/Hero.js
@@ -2,9 +2,6 @@ import React from "react";
 
 import ImageHero from "assets/images/img-hero.jpg";
 import ImageHero_ from "assets/images/img-hero-frame.jpg";
-import IconCities from "assets/images/icons/icon_cities.svg";
-import IconTraveler from "assets/images/icons/icon_traveler.svg";
-import IconTreasure from "assets/images/icons/icon_treasure.svg";
 
 import Fade from "react-reveal/Fade";
 
@@ -48,7 +45,7 @@ export default function Hero(props) {
                 <img
                   width="36"
                   height="36"
-                  src={IconTraveler}
+                  src="../assets/images/icon_traveler.svg"
                   alt={`${props.data.travelers} Travelers`}
                 />
                 <h6 className="mt-3">
@@ -62,7 +59,7 @@ export default function Hero(props) {
                 <img
                   width="36"
                   height="36"
-                  src={IconTreasure}
+                  src="/assets/images/icon_treasure.svg"
                   alt={`${props.data.treasures} Treasures`}
                 />
                 <h6 className="mt-3">
@@ -76,7 +73,7 @@ export default function Hero(props) {
                 <img
                   width="36"
                   height="36"
-                  src={IconCities}
+                  src="/assets/images/icon_cities.svg"
                   alt={`${props.data.travelers} Cities`}
                 />
                 <h6 className="mt-3">
